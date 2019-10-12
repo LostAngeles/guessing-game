24e35858e36611e753e0f70254aca2ccd7eec6bc
LostAngeles <55195328+LostAngeles@users.noreply.github.com>
2019-10-12 23:13:12 +0300

    feat: guessing-game solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.middle = 0;
+        this.first = 0;
+        this.second = 0;
+    }
 
     setRange(min, max) {
-
+        this.first = min;
+        this.second = max;
     }
 
     guess() {
-
+        this.middle = Math.ceil((this.first+this.second)/2);
+        return this.middle; 
     }
 
     lower() {
-
+        this.second = this.middle;
+        return this.second;       
     }
 
     greater() {
-
+        this.first = this.middle;
+        return this.first;
     }
 }
 
